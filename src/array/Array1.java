e7723a950fdda5c9576e0c543567e2370e45b7d9
Dmitry <dmitilyin@gmail.com>
2018-03-02 13:10:18 +0300

    + парочка функций

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -6,6 +6,24 @@ public class Array1 {
 
     }
 
+    public static int[] rotateLeft3(int[] nums) {
+        return new int[]{nums[1], nums[2], nums[0]};
+    }
+
+
+    public static int sum3(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result += num;
+        }
+        return result;
+    }
+
+
+    public boolean commonEnd(int[] a, int[] b) {
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
     public boolean firstLast6(int[] nums) {
         return nums[0] == 6 || nums[nums.length-1] == 6;
     }
