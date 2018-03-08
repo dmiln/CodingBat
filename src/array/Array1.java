25b4a0b729ddea026d52b9f7397e17b495460e1b
Dmitry <dmitilyin@gmail.com>
2018-03-08 17:56:23 +0300

    + закончил Array1

--- src/array/Array1.java
+++ src/array/Array1.java
@@ -13,6 +13,29 @@ public class Array1 {
 
     }
 
+    public int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length != 0){
+            return new int[]{b[0]};
+        }
+        if (b.length == 0 && a.length != 0){
+            return new int[]{a[0]};
+        }
+        if (a.length == 0){
+            return new int[]{};
+        }
+        return new int[]{a[0],b[0]};
+    }
+
+    public int[] make2(int[] a, int[] b) {
+        if (a.length > 1){
+            return new int[]{a[0],a[1]};
+        }else if (a.length == 1){
+            return new int[]{a[0],b[0]};
+        }else{
+            return new int[]{b[0],b[1]};
+        }
+    }
+
     public boolean unlucky1(int[] nums) {
         if (nums.length > 1) {
             if (nums[0] == 1) {
