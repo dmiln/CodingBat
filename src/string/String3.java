2f4e09de668fc0c16df44b0c00629170bc86ad89
Dmitry <dmitilyin@gmail.com>
2018-06-09 16:15:08 +0300

    + начал String3

--- src/string/String3.java
+++ src/string/String3.java
@@ -2,9 +2,8 @@ package string;
 
 public class String3 {
     public static void main(String[] args) {
-        System.out.println(countYZ("day fyyyz"));
-        System.out.println(countYZ("day fyz"));
-        System.out.println(countYZ("day^fyz"));
+        System.out.println(withoutString("xxx", "xx"));
+
     }
 
     private static int countYZ(String str) {
@@ -21,5 +20,22 @@ public class String3 {
             }
         }
         return count;
+    }
+
+    private static String withoutString(String base, String remove) {
+        for (int i = 0; i < base.length(); i++){
+            for (int j = 0; j < remove.length(); j++){
+                if (!(remove.toLowerCase().charAt(j) == base.toLowerCase().charAt(i + j)))
+                    break;
+                if (j == remove.length() - 1){
+                    base = base.substring(0,i) + base.substring(i + j + 1);
+                    --i;
+                }
+            }
+            if (remove.length() > base.length()){
+                break;
+            }
+        }
+        return base;
     }
 }
